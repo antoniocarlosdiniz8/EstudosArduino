60361a8b103155a4081ad0bfca5023c1906d3509
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2022-02-20 18:37:05 -0300

    coloquei comentarios

--- newbuzzer/newbuzzer.ino
+++ newbuzzer/newbuzzer.ino
@@ -1,21 +1,22 @@
-const int pinoSensor = 2;
-const int pinoLed = 12;
+const int pinoSensor = 2;//PINO DIGITAL UTILIZADO PELO SENSOR
+const int pinoLed = 12;//PINO DIGITAL UTILIZADO PELO LED
 
 void setup() {
   // put your setup code here, to run once:
- pinMode(pinoSensor, INPUT_PULLUP);
- pinMode(pinoLed, OUTPUT);
- digitalWrite(pinoLed, LOW);
+ pinMode(pinoSensor, INPUT_PULLUP);//DEFINE O PINO COMO ENTRADA / "_PULLUP" É PARA ATIVAR O RESISTOR INTERNO
+ //DO ARDUINO PARA GARANTIR QUE NÃO EXISTA FLUTUAÇÃO ENTRE 0 (LOW) E 1 (HIGH)
+ pinMode(pinoLed, OUTPUT);//DEFINE O PINO COMO SAÍDA
+ digitalWrite(pinoLed, LOW);//LED INICIA DESLIGADO
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(pinoSensor) == HIGH){
-    digitalWrite(pinoLed, HIGH);
+  if(digitalRead(pinoSensor) == HIGH){//SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
+    digitalWrite(pinoLed, HIGH);//ACENDE O LED
   }
   else{
-    digitalWrite(pinoLed, LOW);
+    digitalWrite(pinoLed, LOW);//APAGA O LED
   }
   
   
